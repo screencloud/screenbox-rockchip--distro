6c46f0a0127fae231564fe0f0923228f2d99555a
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-18 10:01:08 +0800

    package: partinit: fix boot up hang up. use install instead of cp
    Change-Id: I7fc1ffced71d72bcaebc8f6997c9bf1fb65806b1
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/partinit/make.sh
+++ package/partinit/make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cp /sdk/buildroot/package/rockchip/partinit/61-partition-init.rules /lib/udev/rules.d/
-chmod 755 /lib/udev/rules.d/61-partition-init.rules
-cp /sdk/buildroot/package/rockchip/partinit/61-sd-cards-auto-mount.rules /lib/udev/rules.d/
-chmod 755 /lib/udev/rules.d/61-sd-cards-auto-mount.rules
 
-echo -e "/dev/block/by-name/misc\t\t/misc\t\t\temmc\t\tdefaults\t\t0\t0" >> /etc/fstab
-echo -e "/dev/block/by-name/oem\t\t/oem\t\t\t$$RK_OEM_FS_TYPE\t\tdefaults\t\t0\t2" >> /etc/fstab
-echo -e "/dev/block/by-name/userdata\t/userdata\t\t$$RK_USERDATA_FS_TYPE\t\tdefaults\t\t0\t2" >> /etc/fstab
+source /sdk/device/rockchip/.BoardConfig.mk
+install -m 644 /sdk/buildroot/package/rockchip/partinit/61-partition-init.rules /lib/udev/rules.d/
+install -m 644  /sdk/buildroot/package/rockchip/partinit/61-sd-cards-auto-mount.rules /lib/udev/rules.d/
+
+# echo -e "/dev/block/by-name/misc\t\t/misc\t\t\temmc\t\tdefaults\t\t0\t0" >> /etc/fstab
+echo -e "/dev/disk/by-partlabel/oem\t/oem\t\t$RK_OEM_FS_TYPE\tdefaults\t0\t2" > /etc/fstab
+echo -e "/dev/disk/by-partlabel/userdata\t/userdata\t$RK_USERDATA_FS_TYPE\tdefaults\t0\t2" >> /etc/fstab
 
 if [ ! -d oem ];then
 	mkdir oem
