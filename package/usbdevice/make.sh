1383f8a64bc03bd119163e1811379bd9a1f7e0db
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-25 14:12:12 +0800

    package: usbdevice: add usbdevice.service for systemd
    Change-Id: I42ba8c613ba5ab432b3ed8e628c604ed3282da11
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/usbdevice/make.sh
+++ package/usbdevice/make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-install -m 755 -D /sdk/buildroot/package/rockchip/usbdevice/S50usbdevice /etc/init.d/
+install -m 755 -D /sdk/buildroot/package/rockchip/usbdevice/S50usbdevice /usr/bin/
+install -m 644 -D /sdk/distro/package/usbdevice/usbdevice.service /lib/systemd/system/
+systemctl enable usbdevice.service
 install -m 644 -D /sdk/buildroot/package/rockchip/usbdevice/61-usbdevice.rules /lib/udev/rules.d/
 install -m 755 -D /sdk/buildroot/package/rockchip/usbdevice/usbdevice /usr/bin/
