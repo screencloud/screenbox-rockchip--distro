956991e641d8f6ef70bfd5108e0145e960d408ec
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-25 14:11:43 +0800

    package: partinit: add mount-all.service for systemd
    Change-Id: I061022882f565489a05727483dc36da52cde02f3
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/partinit/make.sh
+++ package/partinit/make.sh
@@ -3,8 +3,9 @@
 source /sdk/device/rockchip/.BoardConfig.mk
 install -m 0644 /sdk/buildroot/package/rockchip/partinit/61-partition-init.rules /lib/udev/rules.d/
 install -m 0644 /sdk/buildroot/package/rockchip/partinit/61-sd-cards-auto-mount.rules /lib/udev/rules.d/
-install -m 0755 /sdk/buildroot/package/rockchip/partinit/S21mountall.sh /etc/init.d/
-
+install -m 0755 /sdk/buildroot/package/rockchip/partinit/S21mountall.sh /usr/bin/
+install -m 0644 /sdk/distro/package/partinit/mount-all.service /lib/systemd/system/
+systemctl enable mount-all.service
 # echo -e "/dev/block/by-name/misc\t\t/misc\t\t\temmc\t\tdefaults\t\t0\t0" >> /etc/fstab
 echo -e "/dev/disk/by-partlabel/oem\t/oem\t\t$RK_OEM_FS_TYPE\tdefaults\t0\t2" > /etc/fstab
 echo -e "/dev/disk/by-partlabel/userdata\t/userdata\t$RK_USERDATA_FS_TYPE\tdefaults\t0\t2" >> /etc/fstab
