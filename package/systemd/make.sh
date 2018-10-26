ac58db05ebf15fc18a4db3f5acfa0325d2a64b7d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-26 15:57:09 +0800

    package: systemd: use a new serial-getty@.service
    Change-Id: I4a25e59ad808c6c5b25d38b30211335b2fc47ea4
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/systemd/make.sh
+++ package/systemd/make.sh
@@ -3,10 +3,5 @@ apt-get install -y systemd
 if [ $? -ne 0 ]; then
 	exit 1
 fi
-if [ -f /lib/systemd/system/serial-getty@.service ]; then
-	sed -i '/ExecStart/c\\nExecStart=-/sbin/agetty --a root --keep-baud 115200,38400,9600 %I $TERM' /lib/systemd/system/serial-getty@.service
-fi
 
-if [ $? -ne 0 ]; then
-	exit 1
-fi
+install -m 0644 /sdk/distro/package/systemd/serial-getty@.service /lib/systemd/system/serial-getty@.service
