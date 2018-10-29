d561e4908c2a9bd7990ea0a496704e10aca64b47
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-29 10:17:11 +0800

    package: ums: usr new usb_config
    Change-Id: I548437a609caa519d070e9652995b14214f70147
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/ums/make.sh
+++ package/ums/make.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+CONFIG_FILE=/usr/bin/.usb_config
 
-touch /.ums.en
+if [ ! -e $CONFIG_FILE ];then
+	touch $CONFIG_FILE
+fi
+
+test ! `grep usb_ums_en $CONFIG_FILE` && echo usb_ums_en >> $CONFIG_FILE
