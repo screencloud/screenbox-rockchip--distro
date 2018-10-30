1580e8ca4aa5fc11e80f8a9c61070688df34d567
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-30 14:03:51 +0800

    package: glmark2: build glmark2
    Change-Id: I816e86d9ba151f08533589d490a383b54f0565b9
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/glmark2/make.sh
+++ package/glmark2/make.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-apt-get install -y glmark2
+#apt-get install -y glmark2-es2-wayland
+
+if [ ! -d /sdk/distro/output/build/glmark2 ];then
+	git clone https://github.com/glmark2/glmark2.git  /sdk/distro/output/build/glmark2
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+fi
+
+cd /sdk/distro/output/build/glmark2
+./waf configure --with-flavors=drm-glesv2,wayland-glesv2 --prefix=/usr
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+./waf
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+./waf install
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+cd -
