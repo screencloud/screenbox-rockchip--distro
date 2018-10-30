fe9ddbb6434f5ff47b291e809c64f582371dfa58
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-30 08:57:40 +0800

    package: vim: don't install again if already installed
    Change-Id: I62f626de80daa2fd5cf5de92dd7381478e7a3c66
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/vim/make.sh
+++ package/vim/make.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-apt-get install -y vim-tiny
-if [ $? -ne 0 ]; then
-	exit 1
+
+pkg_name=vim-tiny
+installed=`dpkg -s $pkg_name  | grep Status`
+if [ "$installed" == "Status: install ok installed" ];then
+	echo "$pkg_name already installed"
+else
+        apt-get install -y $pkg_name
+        if [ $? -ne 0 ]; then
+                exit 1
+        fi
 fi
