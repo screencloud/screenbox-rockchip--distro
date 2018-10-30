a45c68ce6a01cce7f071dc6b1968fbc34da31aa0
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-30 09:00:12 +0800

    package: systemd: don't install again if already installed
    Change-Id: I5edd17d86943ee50855f400829230d22301ab2cf
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/systemd/make.sh
+++ package/systemd/make.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-apt-get install -y systemd
-if [ $? -ne 0 ]; then
-	exit 1
+
+pkg_name=systemd
+installed=`dpkg -s $pkg_name  | grep Status`
+if [ "$installed" == "Status: install ok installed" ];then
+	echo "$pkg_name already installed"
+else
+        apt-get install -y $pkg_name
+        if [ $? -ne 0 ]; then
+                exit 1
+        fi
 fi
 
 install -m 0644 /sdk/distro/package/systemd/serial-getty@.service /lib/systemd/system/serial-getty@.service
