f7c99fdd5de63240f0d768f66f443753e2fe5cda
Eddie Cai <eddie.cai.linux@gmail.com>
2018-10-30 11:53:41 +0800

    package: libudev: don't install again if already installed
    Change-Id: I62d577734a5fc445fb0b29396ec306c37107d7f2
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libudev/make.sh
+++ package/libudev/make.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-apt-get install -y libudev-dev
 
-if [ $? -ne 0 ]; then
-	exit 1
+pkg_name=libudev-dev
+installed=`dpkg -s $pkg_name  | grep Status`
+if [ "$installed" == "Status: install ok installed" ];then
+	echo "$pkg_name already installed"
+else
+        apt-get install -y $pkg_name
+        if [ $? -ne 0 ]; then
+                exit 1
+        fi
 fi
