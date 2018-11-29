a947c966c1e789f8ec0097a68d539edeefb5e6cc
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 11:35:50 +0800

    package: weston: remove sudo
    Change-Id: I478d718db1d06191d217e79a57dd6cbe6c335ce3
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/weston/make.sh
+++ package/weston/make.sh
@@ -11,7 +11,7 @@ if [ x$METHOD = xcross ];then
 	export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$SYSROOT/lib:$SYSROOT/usr/lib:$TARGET_DIR/lib/$TOOLCHAIN:$TARGET_DIR/usr/lib:$TARGET_DIR/usr/lib/$TOOLCHAIN -L$TARGET_DIR/usr/lib"
 	./autogen.sh --target=aarch64-linux-gnu --host=aarch64-linux-gnu --disable-dependency-tracking --disable-static --enable-shared  --disable-headless-compositor --disable-colord --disable-devdocs --disable-setuid-install --enable-dbus --enable-weston-launch --enable-egl --disable-rdp-compositor --disable-fbdev-compositor --enable-drm-compositor WESTON_NATIVE_BACKEND=drm-backend.so --disable-x11-compositor --disable-xwayland --disable-vaapi-recorder --disable-lcms --disable-systemd-login --disable-systemd-notify --enable-junit-xml --disable-demo-clients-install
 	make WAYLAND_PROTOCOLS_DATADIR=$TARGET_DIR/usr/share/wayland-protocols
-	sudo -E make install
+	make install
 	cd -
 fi
 
