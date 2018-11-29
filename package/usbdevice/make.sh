55a94ce6c9f0ca0bbf34ac00dde765c2518c551a
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 10:51:06 +0800

    package: usbdevice: remove sudo
    Change-Id: Ib4a7c66aa754d0663516d36119ee7d9a5f868eb5
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/usbdevice/make.sh
+++ package/usbdevice/make.sh
@@ -3,10 +3,10 @@
 METHOD=$1
 if [ x$METHOD = xcross ];then
 	set -e
-	sudo install -m 755 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/S50usbdevice $TARGET_DIR/usr/bin/
-	sudo install -m 644 -D $DISTRO_DIR/package/usbdevice/usbdevice.service $TARGET_DIR/lib/systemd/system/
-	sudo install -m 644 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/61-usbdevice.rules $TARGET_DIR/lib/udev/rules.d/
-	sudo install -m 755 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/usbdevice $TARGET_DIR/usr/bin/
+	install -m 755 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/S50usbdevice $TARGET_DIR/usr/bin/
+	install -m 644 -D $DISTRO_DIR/package/usbdevice/usbdevice.service $TARGET_DIR/lib/systemd/system/
+	install -m 644 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/61-usbdevice.rules $TARGET_DIR/lib/udev/rules.d/
+	install -m 755 -D $TOP_DIR/buildroot/package/rockchip/usbdevice/usbdevice $TARGET_DIR/usr/bin/
 else
 	systemctl enable usbdevice.service
 fi
