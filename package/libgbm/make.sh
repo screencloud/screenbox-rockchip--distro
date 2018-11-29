7321d46cab2303f66de846d91b74ed1d36a32f7d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 10:44:34 +0800

    package: libgbm: modify libgbm version
    weston will define HAVE_GBM_MODIFIERS when libgbm version higher than
    17.1. but rockchip libmali don't have these api which will cause failure
    when use GPU. so modify libgbm version to remove HAVE_GBM_MODIFIERS
    
    Change-Id: I7de8237e66a3b5639cf6b639e1b689a208ec30ce
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libgbm/make.sh
+++ package/libgbm/make.sh
@@ -7,5 +7,8 @@ if [ x$METHOD = xcross ];then
 else
 	set -e
 	/sdk/distro/scripts/install.sh $PACKAGE
+	source /sdk/device/rockchip/.BoardConfig.mk
+	source /sdk/distro/scripts/env_chroot.sh
+	sed -i '9s/18.2.5/17/' /usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
 fi
 
