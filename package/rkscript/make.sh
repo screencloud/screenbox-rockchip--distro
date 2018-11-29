3e572f23f40e9995a04101fcd6be38aedb5a543e
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 10:50:07 +0800

    package: rkscript: remove sudo
    Change-Id: Ic0ba482423e5dccc87b87670a19d1e86d2039f73
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rkscript/make.sh
+++ package/rkscript/make.sh
@@ -2,10 +2,9 @@
 
 set -e
 METHOD=$1
-CURRENT_DIR=$(dirname $(realpath "$0"))
 if [ x$METHOD = xcross ];then
 	
-	sudo install -m 0755 -D $TOP_DIR/buildroot/package/rockchip/rkscript/*.sh $TARGET_DIR/usr/bin/
+	install -m 0755 -D $TOP_DIR/buildroot/package/rockchip/rkscript/*.sh $TARGET_DIR/usr/bin/
 else
 	exit 0
 fi
