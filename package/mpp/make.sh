d7f5bf908f0b05da50275d9474eb324aed4863a2
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 11:36:10 +0800

    package: mpp: use cross compile to build mpp
    Change-Id: I633bf321661e8fda693d07c875d7ec8ef12ab7b9
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/mpp/make.sh
+++ package/mpp/make.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
 set -e
-cd /sdk/external/mpp
-cmake -DRKPLATFORM=ON -DHAVE_DRM=ON .
-make
-make install
-cd -
+METHOD=$1
+export CURRENT_DIR=$(dirname $(realpath "$0"))
+if [ x$METHOD = xcross ];then
+	cd $TOP_DIR/external/mpp
+	cmake -DRKPLATFORM=ON -DHAVE_DRM=ON .
+	make 
+	make install
+	cd -
+fi
