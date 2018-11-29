a017cc4e25e37ca16b58a14c8eae0af0cab9fbdf
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 10:43:48 +0800

    package: libdrm: remove sudo when cross compile
    Change-Id: I8fd06984c6782b3b9c3d5252b3483160859c59b7
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libdrm/make.sh
+++ package/libdrm/make.sh
@@ -7,6 +7,6 @@ if [ x$METHOD = xcross ];then
 	cd $TOP_DIR/external/libdrm
 	./autogen.sh --target=aarch64-linux-gnu --host=aarch64-linux-gnu --disable-dependency-tracking --disable-static --enable-shared  --disable-cairo-tests --disable-manpages --disable-intel --disable-radeon --disable-amdgpu --disable-nouveau --disable-vmwgfx --disable-omap-experimental-api --disable-etnaviv-experimental-api --disable-exynos-experimental-api --disable-freedreno --disable-tegra-experimental-api --disable-vc4 --enable-rockchip-experimental-api --enable-udev --disable-valgrind --enable-install-test-programs
 	make 
-	sudo -E make install
+	make install
 	cd -
 fi
