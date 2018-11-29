1568b43d8c11aa9e04e7512f40f0fc484e294eb7
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-29 10:43:16 +0800

    package: alsa-config: remove sudo
    Change-Id: Iee6e20dce438c78b5876872fdeed16bf6e43a14b
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/alsa-config/make.sh
+++ package/alsa-config/make.sh
@@ -5,7 +5,7 @@ METHOD=$1
 CURRENT_DIR=$(dirname $(realpath "$0"))
 if [ x$METHOD = xcross ];then
 	
-	sudo install -D -m 0644 -D $TOP_DIR/external/alsa-config/cards/* $TARGET_DIR/usr/share/alsa/cards/
+	install -m 0644 -D $TOP_DIR/external/alsa-config/cards/* $TARGET_DIR/usr/share/alsa/cards/
 else
 	exit 0
 fi
