0d9011b6c0a2c069a978c1a7eccdb9a2f882c619
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-30 08:47:14 +0800

    package: libgbm: replace the whole line of version
    Change-Id: Ie5c9b6d501e2b3fada36e185bdd7ae08d0a65d43
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libgbm/make.sh
+++ package/libgbm/make.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 PACKAGE=libgbm-dev
 METHOD=$1
 if [ x$METHOD = xcross ];then
 	exit 0
 else
-	set -e
 	/sdk/distro/scripts/install.sh $PACKAGE
 	source /sdk/device/rockchip/.BoardConfig.mk
 	source /sdk/distro/scripts/env_chroot.sh
-	sed -i '9s/18.2.5/17/' /usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
+	sed -i '9c Version: 17' /usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
 fi
 
