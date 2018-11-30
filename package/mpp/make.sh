46afb4ac3c388ed52aece5cb06cbd15db3cd8375
Eddie Cai <eddie.cai.linux@gmail.com>
2018-11-30 16:06:15 +0800

    package: mpp: add CMAKE_INSTALL_LIBDIR and CMAKE_INSTALL_PREFIX define
    Change-Id: Id5707991ce9cb6c589039e29599ab627640ee6de
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/mpp/make.sh
+++ package/mpp/make.sh
@@ -5,7 +5,7 @@ METHOD=$1
 export CURRENT_DIR=$(dirname $(realpath "$0"))
 if [ x$METHOD = xcross ];then
 	cd $TOP_DIR/external/mpp
-	cmake -DRKPLATFORM=ON -DHAVE_DRM=ON .
+	cmake -DRKPLATFORM=ON -DHAVE_DRM=ON -DCMAKE_INSTALL_LIBDIR=/usr/lib/$TOOLCHAIN -DCMAKE_INSTALL_PREFIX=/usr .
 	make 
 	make install
 	cd -
