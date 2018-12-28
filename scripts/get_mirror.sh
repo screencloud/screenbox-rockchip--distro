7fec5e4cb825952e91c5dc2108b1ddfb656c0c6c
Eddie Cai <eddie.cai.linux@gmail.com>
2018-12-28 18:10:38 +0800

    script: get_mirror.sh: add default mirror support
    Change-Id: I53ac06de06009ac696d8b0f9b0b95997ac287d26
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- scripts/get_mirror.sh
+++ scripts/get_mirror.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-DEBIAN_CDN=http://cdn.debian.net/debian
-UBUNTU_CDN=
-DEFAULT_MIRROR=http://ftp.cn.debian.org/debian
+DEFAULT_DEBIAN_MIRROR=http://ftp.cn.debian.org/debian
+DEFAULT_UBUNTU_MIRROR=http://mirrors.ustc.edu.cn/ubuntu
 NETSELECT_APT_DEB=netselect-apt_0.3.ds1-28_all.deb
 NETSELECT_APT_URL=http://ftp.cn.debian.org/debian/pool/main/n/netselect/$NETSELECT_APT_DEB
 NETSELECT_DEB=netselect_0.3.ds1-28+b1_amd64.deb
 NETSELECT_URL=http://ftp.cn.debian.org/debian/pool/main/n/netselect/$NETSELECT_DEB
+
 OS=$1
 ARCH=$2
-CDN=$3
+
+if [ $3 = default ];then
+	if [ $OS = debian ];then
+		echo "$DEFAULT_DEBIAN_MIRROR"
+		exit 0
+	elif [ $OS = ubuntu ];then
+		echo "$DEFAULT_UBUNTU_MIRROR"
+		exit 0
+	fi
+fi
 
 usage() {
         echo "Usage: $0: distro arch" >&2
@@ -33,22 +42,14 @@ netselect_apt_chkinstall()
 
 main()
 {
-	if [ $OS == debian ];then
-		if [ x$CDN == xcdn ];then
-			MIRROR=$DEBIAN_CDN
-		else
-			netselect_chkinstall
-			netselect_apt_chkinstall
-			MIRROR=`sudo netselect-apt -o /tmp/.sources.list -t 20 -a $ARCH 2>&1 |grep -A 1 "fastest valid for HTTP" | tail -1 | cut -d ' ' -f 9`
-			sudo rm -f /tmp/.sources.list
-		fi
-	elif [ $OS == ubuntu ];then
-		if [ x$CDN == xcdn ];then
-			MIRROR=$UBUNTU_CDN
-		else
-			netselect_chkinstall
-			MIRROR=`sudo netselect -s 1 $(wget -qO - mirrors.ubuntu.com/mirrors.txt) | cut -d ' ' -f 5`
-		fi
+	if [ $OS = debian ];then
+		netselect_chkinstall
+		netselect_apt_chkinstall
+		MIRROR=`sudo netselect-apt -o /tmp/.sources.list -t 20 -a $ARCH 2>&1 |grep -A 1 "fastest valid for HTTP" | tail -1 | cut -d ' ' -f 9`
+		sudo rm -f /tmp/.sources.list
+	elif [ $OS = ubuntu ];then
+		netselect_chkinstall
+		MIRROR=`sudo netselect -s 1 $(wget -qO - mirrors.ubuntu.com/mirrors.txt) | cut -d ' ' -f 5`
 	else
 		usage
 	fi
