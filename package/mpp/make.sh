b66bec790dbb0d1d3f306d463bdf73ca8fe0aa0d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-12-29 16:32:48 +0800

    add mpp support for new build system
    Change-Id: Icf4219059ef255cf62a920e06aab253b371a8d95
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/mpp/make.sh
+++ package/mpp/make.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e
-METHOD=$1
-export CURRENT_DIR=$(dirname $(realpath "$0"))
-if [ x$METHOD = xcross ];then
-	cd $TOP_DIR/external/mpp
-	cmake -DRKPLATFORM=ON -DHAVE_DRM=ON -DCMAKE_INSTALL_LIBDIR=/usr/lib/$TOOLCHAIN -DCMAKE_INSTALL_PREFIX=/usr .
-	make 
-	make install
-	cd -
-fi
+mkdir -p $BUILD_DIR/mpp
+rm -f BUILD_DIR/mpp/CMakeCache.txt
+echo "PATH:$PATH"
+cd $BUILD_DIR/mpp
+cmake -DRKPLATFORM=ON -DHAVE_DRM=ON -DCMAKE_INSTALL_LIBDIR=/usr/lib/$TOOLCHAIN -DCMAKE_INSTALL_PREFIX=/usr $TOP_DIR/external/mpp
+make
+make install
+cd -
