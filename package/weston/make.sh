06e76b2770cdccdc630f55dd59d075a1cdf06ec3
Eddie Cai <eddie.cai.linux@gmail.com>
2019-01-23 13:28:46 +0800

    package: weston: fix build error cause by gbm version change
    Change-Id: I35ac127855b67adebdd08040bd0670e6eaf1b069
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/weston/make.sh
+++ package/weston/make.sh
@@ -10,7 +10,7 @@ fi
 if [ ! -d $BUILD_DIR/$PKG ];then
         tar -xzf $DOWNLOAD_DIR/$PKG.tar.gz -C $BUILD_DIR
 fi
-sed -i '7c Version: 17' $TARGET_DIR/usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
+sed -i 's/^Version.*/Version: 17/g' $TARGET_DIR/usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
 cd $BUILD_DIR/$PKG
 ./autogen.sh --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/usr --libdir=/usr/lib/$TOOLCHAIN --disable-dependency-tracking --disable-static --enable-shared  --disable-headless-compositor --disable-colord --disable-devdocs --disable-setuid-install --enable-dbus --enable-weston-launch --enable-egl --disable-rdp-compositor --disable-fbdev-compositor --enable-drm-compositor WESTON_NATIVE_BACKEND=drm-backend.so --disable-x11-compositor --disable-xwayland --disable-vaapi-recorder --disable-lcms --disable-systemd-login --disable-systemd-notify --enable-junit-xml --disable-demo-clients-install
 make WAYLAND_PROTOCOLS_DATADIR=$TARGET_DIR/usr/share/wayland-protocols
