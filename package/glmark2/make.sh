c3b39a50e4765d83703c608d9ec9d127f0b32541
Eddie Cai <eddie.cai.linux@gmail.com>
2019-01-23 13:27:49 +0800

    package: glmark2: download gz package instead of git repo
    Change-Id: I3a77af32ce97a86ae223dcf00062bd9dc28d9eb4
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/glmark2/make.sh
+++ package/glmark2/make.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 set -e
-if [ ! -d $BUILD_DIR/glmark2 ];then
-	git clone https://github.com/glmark2/glmark2.git  $BUILD_DIR/glmark2
+COMMIT=9b1070fe9c5cf908f323909d3c8cbed08022abe8
+PKG=glmark2-$COMMIT
+if [ ! -d $BUILD_DIR/$PKG ];then
+	wget -O $DOWNLOAD_DIR/$PKG.tar.gz https://github.com/glmark2/glmark2/archive/$COMMIT/$PKG.tar.gz
 fi
 
-cd $BUILD_DIR/glmark2
-echo "cflag:$CFLAGS, cxxflag:$CXXFLAGS LDFLAGS=$LDFLAGS"
+if [ ! -d $BUILD_DIR/$PKG ];then
+	tar -xzf $DOWNLOAD_DIR/$PKG.tar.gz -C $BUILD_DIR
+fi
+
+cd $BUILD_DIR/$PKG
 ./waf configure --with-flavors=drm-glesv2,wayland-glesv2 --prefix=/usr
 ./waf
 ./waf install
