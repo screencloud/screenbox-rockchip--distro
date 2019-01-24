bb29172ab84b268ab1b8796f51083d6b22f650fe
Eddie Cai <eddie.cai.linux@gmail.com>
2019-01-24 14:33:58 +0800

    package: glmark2: use latest glmark2 package to avoid build error
    Change-Id: I8629817b9cc93304599ff8b245385ad17185b041
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/glmark2/make.sh
+++ package/glmark2/make.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-COMMIT=9b1070fe9c5cf908f323909d3c8cbed08022abe8
+COMMIT=9a03892d0ef250b0eb5c87792dbfbd48e23d15bb
 PKG=glmark2-$COMMIT
-if [ ! -d $BUILD_DIR/$PKG ];then
+if [ ! -e $DOWNLOAD_DIR/$PKG.tar.gz ];then
 	wget -O $DOWNLOAD_DIR/$PKG.tar.gz https://github.com/glmark2/glmark2/archive/$COMMIT/$PKG.tar.gz
 fi
 
