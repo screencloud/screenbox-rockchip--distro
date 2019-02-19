c49da5deae8c63867dea1cdd70e20ce2d232bdae
Eddie Cai <eddie.cai.linux@gmail.com>
2019-02-19 09:15:09 +0800

    package: busybox: copy file from buildroot. make busybox independent
    Change-Id: Ib3ac64f203ed6bc1fc87d68a9d4ab1f5a70bb980
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/busybox/make.sh
+++ package/busybox/make.sh
@@ -9,7 +9,7 @@ fi
 
 if [ ! -d $BUILD_DIR/$PKG ];then
 	tar -jxf $DOWNLOAD_DIR/$PKG.tar.bz2 -C $BUILD_DIR
-	$BUILDROOT_DIR/support/scripts/apply-patches.sh $BUILD_DIR/$PKG $TOP_DIR/buildroot/package/busybox
+	$SCRIPTS_DIR/apply-patches.sh $BUILD_DIR/$PKG $PACKAGE_DIR/busybox
 	cp $PACKAGE_DIR/busybox/busybox.config $BUILD_DIR/$PKG/.config
 fi
 
@@ -27,19 +27,21 @@ cd -
 install -m 0644 -D $PACKAGE_DIR/busybox/inittab $TARGET_DIR/etc/inittab
 
 if grep -q CONFIG_UDHCPC=y $BUILD_DIR/$PKG/.config; then
-	install -m 0755 -D $BUILDROOT_PKG_DIR/busybox/udhcpc.script $TARGET_DIR/usr/share/udhcpc/default.script; 
+	install -m 0755 -D $PACKAGE_DIR/busybox/udhcpc.script $TARGET_DIR/usr/share/udhcpc/default.script; 
 	install -m 0755 -d $TARGET_DIR/usr/share/udhcpc/default.script.d; 
 fi
 
 if grep -q CONFIG_SYSLOGD=y $BUILD_DIR/$PKG/.config; then
-	install -m 0755 -D $BUILDROOT_PKG_DIR/busybox/S01logging $TARGET_DIR/etc/init.d/S01logging
+	install -m 0755 -D $PACKAGE_DIR/busybox/S01logging $TARGET_DIR/etc/init.d/S01logging
 fi
 
 if grep -q CONFIG_FEATURE_TELNETD_STANDALONE=y $BUILD_DIR/$PKG/.config; then
-	install -m 0755 -D $BUILDROOT_PKG_DIR/busybox/S50telnet $TARGET_DIR/etc/init.d/S50telnet
+	install -m 0755 -D $PACKAGE_DIR/busybox/S50telnet $TARGET_DIR/etc/init.d/S50telnet
 fi
 
-install -D -m 0755 $BUILDROOT_PKG_DIR/initscripts/init.d/* $TARGET_DIR/etc/init.d/
+install -D -m 0755 $PACKAGE_DIR/busybox/rcK $TARGET_DIR/etc/init.d/
+install -D -m 0755 $PACKAGE_DIR/busybox/rcS $TARGET_DIR/etc/init.d/
+install -D -m 0755 $PACKAGE_DIR/busybox/S20urandom $TARGET_DIR/etc/init.d/
 if [ -e $TARGET_DIR/etc/init.d/udev ];then
 	mv $TARGET_DIR/etc/init.d/udev $TARGET_DIR/etc/init.d/S10udev
 fi
