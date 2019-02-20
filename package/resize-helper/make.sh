ee03f5b05fe1443ca5299810ebc92388dbe3d2a4
Eddie Cai <eddie.cai.linux@gmail.com>
2019-02-20 16:24:03 +0800

    package: rkscript: merge ums, adbd, resize-help to rkscript
    Change-Id: Ib5d599d91408fad9d8fa04fadb4330110e25103c
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/resize-helper/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-install -m 0755 -D $PACKAGE_DIR/resize-helper/resize-helper $TARGET_DIR/usr/sbin/resize-helper
-install -m 0755 -D $PACKAGE_DIR/resize-helper/S22resize-disk $TARGET_DIR/etc/init.d/
