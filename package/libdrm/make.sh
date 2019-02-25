8eeb1888d8589bff9624c67aa9e6e1b243cc5d4a
Eddie Cai <eddie.cai.linux@gmail.com>
2019-02-25 10:30:57 +0800

    add libdrm-rockchip
    Change-Id: Ibe821c65e775671eac9de64ae12a2c0b97317ec3
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libdrm/make.sh
+++ package/libdrm/make.sh
@@ -4,7 +4,8 @@ set -e
 PKG=libdrm
 mkdir -p $BUILD_DIR/$PKG
 cd $BUILD_DIR/$PKG
-$TOP_DIR/external/libdrm/autogen.sh --srcdir=$TOP_DIR/external/libdrm --target=aarch64-linux-gnu --host=aarch64-linux-gnu --disable-dependency-tracking --disable-static --enable-shared  --disable-cairo-tests --disable-manpages --disable-intel --disable-radeon --disable-amdgpu --disable-nouveau --disable-vmwgfx --disable-omap-experimental-api --disable-etnaviv-experimental-api --disable-exynos-experimental-api --disable-freedreno --disable-tegra-experimental-api --disable-vc4 --enable-rockchip-experimental-api --enable-udev --disable-valgrind --enable-install-test-programs
+$TOP_DIR/external/libdrm/autogen.sh --srcdir=$TOP_DIR/external/libdrm --libdir=/usr/lib/$TOOLCHAIN --includedir=/usr/include --target=aarch64-linux-gnu --host=aarch64-linux-gnu --disable-dependency-tracking --disable-static --enable-shared  --disable-cairo-tests --disable-manpages --disable-intel --disable-radeon --disable-amdgpu --disable-nouveau --disable-vmwgfx --disable-omap-experimental-api --disable-etnaviv-experimental-api --disable-exynos-experimental-api --disable-freedreno --disable-tegra-experimental-api --disable-vc4 --enable-rockchip-experimental-api --enable-udev --disable-valgrind --enable-install-test-programs
 make 
 make install
+$SCRIPTS_DIR/fixlibtool.sh $TARGET_DIR $TARGET_DIR
 cd -
