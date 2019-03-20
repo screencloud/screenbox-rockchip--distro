294e938d3b9eba05a669fa7945904ee07275c318
Eddie Cai <eddie.cai.linux@gmail.com>
2019-03-20 09:36:53 +0800

    package: rkwifibt: add dependence and build it
    Change-Id: I5c6834febd79bfc9711885a5c23f7208ed7a9392
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rkwifibt/make.sh
+++ package/rkwifibt/make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+DEPENDENCIES="wpasupplicant dhcpcd5 ntp ntpdate"
+$SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE $MIRROR "$DEPENDENCIES"
 source $OUTPUT_DIR/.config
 if [ -e $TOP_DIR/build.sh ] && [ ! -e $OUTPUT_DIR/.kernelmodules.done ];then
 	export LDFLAGS="--sysroot=$SYSROOT"
