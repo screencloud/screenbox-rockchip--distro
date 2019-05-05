aba71dc0254ce75773dc7b60e7bde6f1af853832
Eddie Cai <eddie.cai.linux@gmail.com>
2019-05-05 10:23:21 +0800

    package: QLauncher: use new QLauncher
    Change-Id: I12ed7f15ec87ea2b40673b68aa380138c157bfdb
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/QLauncher/make.sh
+++ package/QLauncher/make.sh
@@ -11,9 +11,9 @@ mkdir -p $BUILD_DIR/$PKG
 cd $BUILD_DIR/$PKG
 $QMAKE $TOP_DIR/app/$PKG
 make
-mkdir -p $TARGET_DIR/usr/local/$PKG
-cp $TOP_DIR/app/QLauncher/resources/images/* $TARGET_DIR/usr/local/QLauncher/
-install -m 0755 -D $BUILD_DIR/$PKG/$PKG $TARGET_DIR/usr/local/$PKG/$PKG
+mkdir -p $TARGET_DIR/usr/share/backgrounds
+cp $TOP_DIR/app/QLauncher/background.jpg $TARGET_DIR/usr/share/backgrounds/
+install -m 0755 -D $BUILD_DIR/$PKG/$PKG $TARGET_DIR/usr/bin/$PKG
 install -m 0755 -D $PACKAGE_DIR/$PKG/S50launcher $TARGET_DIR/etc/init.d/S50launcher
 cd -
 
