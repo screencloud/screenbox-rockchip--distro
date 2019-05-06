2c7c0318a5880e2c35637e60dddd40bdd5db4928
Eddie Cai <eddie.cai.linux@gmail.com>
2019-05-06 17:15:35 +0800

    scripts: install_ubuntu_pkg.sh: compatiable with 32 and 64 qemu
    Change-Id: I350c7e2beab06f082b1a83aaef91033606610ea3
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- scripts/install_ubuntu_pkg.sh
+++ scripts/install_ubuntu_pkg.sh
@@ -17,18 +17,28 @@ run() {
     "$@"
 }
 
+if [ x$ARCH == xarm64 ]; then
+        qemu_arch=aarch64
+elif [ x$arch == xarm ]; then
+        qemu_arch=arm
+else
+        echo "wrong arch parameter, it should be arm or arm64"
+fi
+
+QEMU=qemu-$qemu_arch-static
 
 if [ x$INIT == xinit ];then
 	if [ ! -d BUILD_DIR/base/ ];then
 		run $SCRIPTS_DIR/get_ubuntu_base.sh $ARCH $SUITE $TARGET_DIR
-		run proot -q qemu-aarch64-static -v -1 -0 -b /dev -b /sys -b /proc -r output/target/ apt-get update
+		run proot -q $QEMU -v -1 -0 -b /dev -b /sys -b /proc -r output/target/ apt-get update
 #		run $SCRIPTS_DIR/chmount.sh -m $TARGET_DIR
 #		run sudo chroot $TARGET_DIR apt-get update
 #		run $SCRIPTS_DIR/chmount.sh -u $TARGET_DIR
 		sleep 1
 	fi
 fi
-run proot -q qemu-aarch64-static -v -1 -0 -b /dev -b /sys -b /proc -r output/target/ apt-get install -y $PKG
+
+run proot -q $QEMU -v -1 -0 -b /dev -b /sys -b /proc -r output/target/ apt-get install -y $PKG
 #run $SCRIPTS_DIR/chmount.sh -m $TARGET_DIR
 #run sudo chroot $TARGET_DIR apt-get install -y $PKG
 #run $SCRIPTS_DIR/chmount.sh -u $TARGET_DIR
