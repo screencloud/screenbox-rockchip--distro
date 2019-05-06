b16b0d44bc77c4c03a55c6c635362331f30797aa
Eddie Cai <eddie.cai.linux@gmail.com>
2019-05-06 16:58:50 +0800

    package: libmpg123: move sample code to package to meet diferent distro
    Change-Id: Id004e5f61fa0ff89807e7d805956629ba13783bf
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/libmpg123/make.sh
+++ package/libmpg123/make.sh
@@ -3,17 +3,8 @@
 set -e
 DEPENDENCIES=libmpg123-dev
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
-MPG123_TO_OUT123_GZ=$TARGET_DIR/usr/share/doc/libmpg123-dev/examples/mpg123_to_out123.c.gz
-MPG123_TO_OUT123_C=$TARGET_DIR/usr/share/doc/libmpg123-dev/examples/mpg123_to_out123.c
-MPG123_TO_WAV_C=$TARGET_DIR/usr/share/doc/libmpg123-dev/examples/mpg123_to_wav.c
-MPGLIB=$TARGET_DIR/usr/share/doc/libmpg123-dev/examples/mpglib.c
-if [ -e $MPG123_TO_OUT123_GZ ];then
-	gzip -dkf $MPG123_TO_OUT123_GZ
-fi
-
-if [ -e $MPG123_TO_OUT123_C ];then
-	cp $MPG123_TO_OUT123_C $MPG123_TO_WAV_C
-fi
+MPG123_TO_WAV_C=$DISTRO_DIR/package/libmpg123/mpg123_to_wav.c
+MPGLIB=$DISTRO_DIR/package/libmpg123/mpglib.c
 
 $GCC $MPG123_TO_WAV_C --sysroot=$TARGET_DIR -lmpg123 -lout123 -lm -ldl -I$TARGET_DIR/usr/include -I$TARGET_DIR/usr/include/$TOOLCHAIN -o $TARGET_DIR/usr/bin/mpg123_to_wav
 
