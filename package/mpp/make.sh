4f8bb285a85eba378beeef57edd17343f7fcd9c2
Eddie Cai <eddie.cai.linux@gmail.com>
2019-05-07 13:57:47 +0800

    package: mpp: fix delete error
    Change-Id: I86e6a04d4dd9192b4daa2a085852e082673e45c7
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/mpp/make.sh
+++ package/mpp/make.sh
@@ -4,7 +4,7 @@ set -e
 DEPENDENCIES=libdrm
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 mkdir -p $BUILD_DIR/mpp
-rm -f BUILD_DIR/mpp/CMakeCache.txt
+rm -f $BUILD_DIR/mpp/CMakeCache.txt
 cd $BUILD_DIR/mpp
 cmake -DRKPLATFORM=ON -DHAVE_DRM=ON -DCMAKE_INSTALL_LIBDIR=/usr/lib/$TOOLCHAIN -DCMAKE_INSTALL_PREFIX=/usr $TOP_DIR/external/mpp
 make
