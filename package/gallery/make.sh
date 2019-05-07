ac1960c7ac90acbadb884ff30f07e57de3d97f29
Eddie Cai <eddie.cai.linux@gmail.com>
2019-05-07 13:57:19 +0800

    package: add dependence for music, video and gallery
    Change-Id: I3ba647c7db98e8de3c161524b175507613aab00b
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/gallery/make.sh
+++ package/gallery/make.sh
@@ -2,6 +2,8 @@
 
 set -e
 PKG=gallery
+DEPENDENCIES="weston libqt5widgets5 libatomic1 qtwayland5"
+$SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 #QMAKE=/usr/bin/qmake
 QMAKE=$TOP_DIR/buildroot/output/rockchip_rk3399/host/bin/qmake
 mkdir -p $BUILD_DIR/$PKG
@@ -10,6 +12,7 @@ $QMAKE $TOP_DIR/app/$PKG
 make
 mkdir -p $TARGET_DIR/usr/local/$PKG
 cp $TOP_DIR/app/$PKG/conf/* $TARGET_DIR/usr/local/$PKG/
+mkdir -p $TARGET_DIR/usr/share/applications
 install -m 0644 -D $TOP_DIR/app/$PKG/gallery.desktop $TARGET_DIR/usr/share/applications/
 install -m 0755 -D $BUILD_DIR/$PKG/galleryView $TARGET_DIR/usr/local/$PKG/galleryView
 cd -
--- package/video/make.sh
+++ package/video/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DEPENDENCIES=gstreamer-rockchip
+DEPENDENCIES="gstreamer-rockchip weston libqt5widgets5 libatomic1 qtwayland5 libqt5multimedia5"
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 PKG=video
 #QMAKE=/usr/bin/qmake
@@ -12,6 +12,7 @@ $QMAKE $TOP_DIR/app/$PKG
 make
 mkdir -p $TARGET_DIR/usr/local/$PKG
 cp $TOP_DIR/app/$PKG/conf/* $TARGET_DIR/usr/local/$PKG/
+mkdir -p $TARGET_DIR/usr/share/applications
 install -m 0644 -D $TOP_DIR/app/$PKG/video.desktop $TARGET_DIR/usr/share/applications/
 install -m 0755 -D $BUILD_DIR/$PKG/videoPlayer $TARGET_DIR/usr/local/$PKG/videoPlayer
 cd -
