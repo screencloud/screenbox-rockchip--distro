85c31c4cb996e06f5f1d82c04d7744bc072f9a23
Ziyuan Xu <xzy.xu@rock-chips.com>
2019-06-27 09:34:39 +0800

    package: music: add libqt5multimedia5-plugins dependency
    Fixup the multimedia error "defaultServiceProvider::requestService():
    no service found for - "org.qt-project.qt.mediaplayer"".
    
    Change-Id: I470346235f7dbd4b49bb5e61faa00eeac628a834
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- package/music/make.sh
+++ package/music/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DEPENDENCIES="gstreamer weston libqt5widgets5 libatomic1 qtwayland5 libqt5multimedia5 audioservice"
+DEPENDENCIES="gstreamer weston libqt5widgets5 libatomic1 qtwayland5 libqt5multimedia5 audioservice libqt5multimedia5-plugins"
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 PKG=music
 #QMAKE=/usr/bin/qmake
