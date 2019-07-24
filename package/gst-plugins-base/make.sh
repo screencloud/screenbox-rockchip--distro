cd794c0a5cb25307666434059e3ebd28dfd3ff86
Ziyuan Xu <xzy.xu@rock-chips.com>
2019-07-24 09:59:53 +0800

    package: gst-plugins-base: fixup dependency
    Fixes commit 93c1e441f96b("package: gst-plugins-base: add dependency")
    
    Change-Id: I041eba3c777f1a4eb574b3594c010dc83f4dcf0b
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- package/gst-plugins-base/make.sh
+++ package/gst-plugins-base/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DEPENDENCIES="libdrm-dev libwayland-client0 gstreamer"
+DEPENDENCIES="libdrm libwayland-dev wayland-protocols libwayland-client0 gstreamer"
 PKG=gst-plugins-base
 VERSION=1.14.4
 source $OUTPUT_DIR/.config
