ac976e2046089bd5d71a040bb117b841320b9d33
Ziyuan Xu <xzy.xu@rock-chips.com>
2019-10-08 11:30:00 +0800

    package: camera_engine_rkisp: use rkisp_3A_server daemon
    Change-Id: Ieea26bc57af94ccc47581f94bf60c32a98039388
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- package/camera_engine_rkisp/make.sh
+++ package/camera_engine_rkisp/make.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ $RK_ARCH = arm ];then
+CAMERA_ENGINE_RKISP_LIB=lib32
+elif [ $RK_ARCH = arm64 ];then
+CAMERA_ENGINE_RKISP_LIB=lib64
+fi
+
 PKG=camera_engine_rkisp
 RKafDir=$TARGET_DIR/usr/lib/rkisp/af
 RKaeDir=$TARGET_DIR/usr/lib/rkisp/ae
@@ -15,16 +21,13 @@ mkdir -p $TARGET_DIR/etc/iqfiles
 
 cd $TOP_DIR/external/$PKG
 make ARCH=$ARCH OUTDIR=$BUILD_DIR/$PKG/build -j$RK_JOBS
+install -D -m 755 $DISTRO_DIR/package/camera_engine_rkisp/S40rkisp_3A $TARGET_DIR/etc/init.d/
 install -D -m 755 $BUILD_DIR/$PKG/build/bin/rkisp_demo $TARGET_DIR/usr/local/bin/
+install -D -m 755 $BUILD_DIR/$PKG/build/bin/rkisp_3A_server $TARGET_DIR/usr/local/bin/
 install -D -m 644 $TOP_DIR/external/$PKG/iqfiles/*.xml $TARGET_DIR/etc/iqfiles/
 install -D -m 644 $BUILD_DIR/$PKG/build/lib/librkisp.so $TARGET_DIR/usr/lib/
-if [ $RK_ARCH = arm ];then
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/af/lib32/librkisp_af.so $RKafDir/
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/aec/lib32/librkisp_aec.so $RKaeDir/
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/awb/lib32/librkisp_awb.so $RKawbDir/
-elif [ $RK_ARCH = arm64 ];then
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/af/lib64/librkisp_af.so $RKafDir/
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/aec/lib64/librkisp_aec.so $RKaeDir/
-	install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/awb/lib64/librkisp_awb.so $RKawbDir/
-fi
+
+install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/af/$CAMERA_ENGINE_RKISP_LIB/librkisp_af.so $RKafDir/
+install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/aec/$CAMERA_ENGINE_RKISP_LIB/librkisp_aec.so $RKaeDir/
+install -D -m 644 $TOP_DIR/external/$PKG/plugins/3a/rkiq/awb/$CAMERA_ENGINE_RKISP_LIB/librkisp_awb.so $RKawbDir/
 cd -
