3ee5d91e763e779113f6da725fd8a992bf0a9c89
Eddie Cai <eddie.cai.linux@gmail.com>
2019-10-09 09:11:26 +0800

    fix qsetting build error
    Change-Id: I84a64277f8e1d9ce21cf970bc0406fd4b4e839c5
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/qsetting/make.sh
+++ package/qsetting/make.sh
@@ -11,7 +11,7 @@ cd $BUILD_DIR/$PKG
 $QMAKE $TOP_DIR/app/$PKG
 make -j$RK_JOBS
 mkdir -p $TARGET_DIR/usr/share/icon
-cp $TOP_DIR/app/$PKG/image/icon_qsetting.png $TARGET_DIR/usr/share/icon/
+cp $TOP_DIR/app/$PKG/icon_qsetting.png $TARGET_DIR/usr/share/icon/
 mkdir -p $TARGET_DIR/usr/share/applications
 install -m 0644 -D $TOP_DIR/app/$PKG/qsetting.desktop $TARGET_DIR/usr/share/applications/
 install -m 0755 -D $BUILD_DIR/$PKG/qsetting $TARGET_DIR/usr/bin/qsetting
