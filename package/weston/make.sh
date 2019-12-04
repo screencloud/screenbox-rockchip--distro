a65924e640f2ae8414845e5ca93b3a3d1fd541cd
Eddie Cai <eddie.cai.linux@gmail.com>
2019-12-04 18:04:19 +0800

    package: weston: change to the same version as buildroot
    Change-Id: I49d85fb2f1b9b4f2f6452f8f11095c7800b8a363
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/weston/make.sh
+++ package/weston/make.sh
@@ -5,15 +5,16 @@ DEPENDENCIES="libdrm libpng-dev libjpeg-dev libegl1-mesa-dev libgles2-mesa-dev l
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 PKG=weston
 VERSION=3.0.0
-if [ ! -e $DOWNLOAD_DIR/$PKG-$VERSION.tar.gz ];then
-        wget -O $DOWNLOAD_DIR/$PKG-$VERSION.tar.gz https://github.com/wayland-project/weston/archive/$VERSION.tar.gz
+if [ ! -e $DOWNLOAD_DIR/$PKG-$VERSION.tar.xz ];then
+        wget -O $DOWNLOAD_DIR/$PKG-$VERSION.tar.xz https://wayland.freedesktop.org/releases/$PKG-$VERSION.tar.xz
 fi
 
 if [ ! -d $BUILD_DIR/$PKG-$VERSION ];then
-        tar -xzf $DOWNLOAD_DIR/$PKG-$VERSION.tar.gz -C $BUILD_DIR
+	tar -xf $DOWNLOAD_DIR/$PKG-$VERSION.tar.xz -C $BUILD_DIR/$PKG
+	mv $BUILD_DIR/$PKG/$PKG-$VERSION/* $BUILD_DIR/$PKG/
 fi
-sed -i 's/^Version.*/Version: 17/g' $TARGET_DIR/usr/lib/$TOOLCHAIN/pkgconfig/gbm.pc
-cd $BUILD_DIR/$PKG-$VERSION
+
+cd $BUILD_DIR/$PKG
 if [ -d $DISTRO_DIR/package/$PKG/$VERSION ]; then
 	for p in $(ls $DISTRO_DIR/package/$PKG/$VERSION/*.patch); do
 		echo "apply patch: "$p
@@ -21,7 +22,7 @@ if [ -d $DISTRO_DIR/package/$PKG/$VERSION ]; then
 	done
 fi
 
-./autogen.sh --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/usr --libdir=/usr/lib/$TOOLCHAIN --disable-dependency-tracking --disable-static --enable-shared  --disable-headless-compositor --disable-colord --disable-devdocs --disable-setuid-install --enable-dbus --enable-weston-launch --enable-egl --disable-rdp-compositor --disable-fbdev-compositor --enable-drm-compositor WESTON_NATIVE_BACKEND=drm-backend.so --disable-x11-compositor --disable-xwayland --disable-vaapi-recorder --disable-lcms --disable-systemd-login --disable-systemd-notify --enable-junit-xml --disable-demo-clients-install
+./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/usr --libdir=/usr/lib/$TOOLCHAIN --disable-dependency-tracking --disable-static --enable-shared  --disable-headless-compositor --disable-colord --disable-devdocs --disable-setuid-install --enable-dbus --enable-weston-launch --enable-egl --disable-rdp-compositor --disable-fbdev-compositor --enable-drm-compositor WESTON_NATIVE_BACKEND=drm-backend.so --disable-x11-compositor --disable-xwayland --disable-vaapi-recorder --disable-lcms --disable-systemd-login --disable-systemd-notify --enable-junit-xml --disable-demo-clients-install
 make WAYLAND_PROTOCOLS_DATADIR=$TARGET_DIR/usr/share/wayland-protocols -j$RK_JOBS
 make install
 cd -
