b6f4ac9d2fcebbae745037e7417d092a20cc752f
Eddie Cai <eddie.cai.linux@gmail.com>
2019-12-06 16:39:37 +0800

    scripts: add install.sh script
    Change-Id: I7999d56ca0a15eb34f7db043a4a532e463cc5837
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,57 @@
 #!/bin/bash
 
-pkg_name=$1
-installed=`dpkg -s $pkg_name  | grep Status`
-if [ "$installed" == "Status: install ok installed" ];then
-	echo "$pkg_name already installed"
-else
-        apt-get install -y $pkg_name
-        if [ $? -ne 0 ]; then
-                exit 1
-        fi
+set -e
+ARCH=$1
+SUITE=$2
+PKGS=$3
+INIT=$4
+
+log() {
+    local format="$1"
+    shift
+    printf -- "$format\n" "$@" >&2
+}
+
+run() {
+    log "I: Running command: %s" "$*"
+    "$@"
+}
+
+OS=`$SCRIPTS_DIR/get_distro.sh $SUITE`
+pkgs=""
+echo "installing package $PKGS"
+for p in $PKGS;do
+	if [ -e $BUILD_DIR/$p/.timestamp ];then
+		if [ -z `find $BUILD_DIR/$p -newer $BUILD_DIR/$p/.timestamp` ];then
+			echo "$p installed before, skiped"
+			continue
+		fi
+	fi
+	pkgs="$pkgs $p"
+done
+
+if [ -z "$pkgs" ];then
+	echo "all packages installed, skip"
+	exit 0
 fi
+
+if [ x$INIT == "xinit" ];then
+       	if [ $OS == debian ];then
+		run $SCRIPTS_DIR/install_debian_pkg.sh $RK_ARCH $SUITE "$pkgs" $INIT
+	elif [ $OS == ubuntu ];then
+		run $SCRIPTS_DIR/install_ubuntu_pkg.sh $RK_ARCH $SUITE "$pkgs" $INIT
+	fi
+else
+	if [ $OS == debian ];then
+		run $SCRIPTS_DIR/install_debian_pkg.sh $RK_ARCH $SUITE "$pkgs"
+	elif [ $OS == ubuntu ];then
+		run $SCRIPTS_DIR/install_ubuntu_pkg.sh $RK_ARCH $SUITE "$pkgs"
+	fi
+fi
+
+for p in $pkgs;do
+	mkdir -p $BUILD_DIR/$p
+	touch $BUILD_DIR/$p/.timestamp
+done
+
+echo "install $pkgs done!!!"
