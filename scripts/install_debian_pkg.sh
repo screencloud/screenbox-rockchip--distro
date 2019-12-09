1611d2b97188cb238bd4e37ae9d1ec3e79e934b9
Eddie Cai <eddie.cai.linux@gmail.com>
2019-12-09 17:54:38 +0800

    scripts: remove multistrap.conf
    Change-Id: Iefc270a4c86bc34ebd0fab88e9fcac588c4d5604
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- scripts/install_debian_pkg.sh
+++ scripts/install_debian_pkg.sh
@@ -7,7 +7,6 @@ AUTH=$4
 set -e
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C
 export PATH=$PATH:/usr/sbin:/sbin
-export QEMU_LD_PREFIX="`readlink -m "$TARGET_DIR"`"
 export PROOT_NO_SECCOMP=1
 if [ $ARCH == arm64 ];then
         QEMU_ARCH=aarch64
@@ -17,29 +16,44 @@ else
         echo "$ARCH is not a valid arch. we only support arm and arm64! set to arm64"
         QEMU_ARCH=aarch64
 fi
-QEMU=qemu-$QEMU_ARCH-static
-CHROOTQEMUCMD="proot -q $QEMU -v -1 -0 -b /dev -b /sys -b /proc -r"
-CHROOTCMD="proot -v -1 -0 -r"
 
+CHROOTQEMUCMD="proot -q qemu-$QEMU_ARCH-static -v -1 -0 -b /dev -b /sys -b /proc -r"
+CHROOTCMD="proot -v -1 -0 -r"
 if [ ! -e $OUTPUT_DIR/.mirror ];then
 	export MIRROR=`$SCRIPTS_DIR/get_mirror.sh $SUITE $ARCH default`
-#	export MIRROR=`$SCRIPTS_DIR/get_mirror.sh $SUITE $ARCH`
 	echo $MIRROR > $OUTPUT_DIR/.mirror
 else
 	export MIRROR=`cat $OUTPUT_DIR/.mirror`
 fi
-ROOTDIR=$TARGET_DIR
-MULTISTRAPCONF=`tempfile -d /tmp -p multistrap`
-echo -n > "$MULTISTRAPCONF"
-while read line; do
-        eval echo $line >> "$MULTISTRAPCONF"
-done < $SCRIPTS_DIR/multistrap.conf
+
+echo "[General]" > $OUTPUT_DIR/multistrap.conf
+#echo "arch=$ARCH" >> $OUTPUT_DIR/multistrap.conf
+#echo "directory=$TARGET_DIR" >> $OUTPUT_DIR/multistrap.conf
+echo "cleanup=true" >> $OUTPUT_DIR/multistrap.conf
+echo "unpack=true" >> $OUTPUT_DIR/multistrap.conf
+echo "bootstrap=Debian_bootstrap" >> $OUTPUT_DIR/multistrap.conf
+echo "aptsources=Debian" >> $OUTPUT_DIR/multistrap.conf
+echo "allowrecommends=false" >> $OUTPUT_DIR/multistrap.conf
+echo "addimportant=false" >> $OUTPUT_DIR/multistrap.conf
+echo "" >> $OUTPUT_DIR/multistrap.conf
+echo "[Debian_bootstrap]" >> $OUTPUT_DIR/multistrap.conf
+echo "packages=$PACKAGES" >> $OUTPUT_DIR/multistrap.conf
+echo "source=$MIRROR" >> $OUTPUT_DIR/multistrap.conf
+echo "suite=$SUITE" >> $OUTPUT_DIR/multistrap.conf
+echo "omitdebsrc=true" >> $OUTPUT_DIR/multistrap.conf
+echo "" >> $OUTPUT_DIR/multistrap.conf
+echo "[Debian]" >> $OUTPUT_DIR/multistrap.conf
+echo "source=http://cdn.debian.net/debian" >> $OUTPUT_DIR/multistrap.conf
+echo "keyring=debian-archive-keyring" >> $OUTPUT_DIR/multistrap.conf
+echo "suite=$SUITE" >> $OUTPUT_DIR/multistrap.conf
+echo "omitdebsrc=true" >> $OUTPUT_DIR/multistrap.conf
+echo "" >> $OUTPUT_DIR/multistrap.conf
 
 if [ x$AUTH == "xinit" ];then
-	proot -0 multistrap -f "$MULTISTRAPCONF"
+	proot -0 multistrap -a $ARCH -d $TARGET_DIR -f $OUTPUT_DIR/multistrap.conf
 	echo "deb [arch=$ARCH] $MIRROR $SUITE main" > $TARGET_DIR/etc/apt/sources.list.d/multistrap-debian.list
 else
-	proot -0 multistrap --no-auth -f "$MULTISTRAPCONF"
+	proot -0 multistrap -a $ARCH -d $TARGET_DIR --no-auth -f $OUTPUT_DIR/multistrap.conf
 fi
 
 cp $SCRIPTS_DIR/debconfseed.txt $TARGET_DIR/tmp/
