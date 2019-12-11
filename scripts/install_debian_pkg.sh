fc3ccb8b1ec84ad64b0c75c97a4c61e2da3c7a24
Eddie Cai <eddie.cai.linux@gmail.com>
2019-12-11 17:00:12 +0800

    scripts: install_debian_pkg.sh: save install log in output/log
    Change-Id: I797ad5dbcf8aece8b94df701fcc4da74ecdf7c97
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- scripts/install_debian_pkg.sh
+++ scripts/install_debian_pkg.sh
@@ -27,45 +27,38 @@ else
 fi
 
 echo "[General]" > $OUTPUT_DIR/multistrap.conf
-#echo "arch=$ARCH" >> $OUTPUT_DIR/multistrap.conf
-#echo "directory=$TARGET_DIR" >> $OUTPUT_DIR/multistrap.conf
+echo "arch=$ARCH" >> $OUTPUT_DIR/multistrap.conf
+echo "directory=$TARGET_DIR" >> $OUTPUT_DIR/multistrap.conf
 echo "cleanup=true" >> $OUTPUT_DIR/multistrap.conf
 echo "unpack=true" >> $OUTPUT_DIR/multistrap.conf
-echo "bootstrap=Debian_bootstrap" >> $OUTPUT_DIR/multistrap.conf
+echo "bootstrap=Debian" >> $OUTPUT_DIR/multistrap.conf
 echo "aptsources=Debian" >> $OUTPUT_DIR/multistrap.conf
 echo "allowrecommends=false" >> $OUTPUT_DIR/multistrap.conf
 echo "addimportant=false" >> $OUTPUT_DIR/multistrap.conf
 echo "" >> $OUTPUT_DIR/multistrap.conf
-echo "[Debian_bootstrap]" >> $OUTPUT_DIR/multistrap.conf
+echo "[Debian]" >> $OUTPUT_DIR/multistrap.conf
 echo "packages=$PACKAGES" >> $OUTPUT_DIR/multistrap.conf
 echo "source=$MIRROR" >> $OUTPUT_DIR/multistrap.conf
-echo "suite=$SUITE" >> $OUTPUT_DIR/multistrap.conf
-echo "omitdebsrc=true" >> $OUTPUT_DIR/multistrap.conf
-echo "" >> $OUTPUT_DIR/multistrap.conf
-echo "[Debian]" >> $OUTPUT_DIR/multistrap.conf
-echo "source=http://cdn.debian.net/debian" >> $OUTPUT_DIR/multistrap.conf
 echo "keyring=debian-archive-keyring" >> $OUTPUT_DIR/multistrap.conf
 echo "suite=$SUITE" >> $OUTPUT_DIR/multistrap.conf
 echo "omitdebsrc=true" >> $OUTPUT_DIR/multistrap.conf
+[ x$AUTH != "xinit" ] && `echo "noauth=true" >> $OUTPUT_DIR/multistrap.conf` || true
 echo "" >> $OUTPUT_DIR/multistrap.conf
 
-if [ x$AUTH == "xinit" ];then
-	proot -0 multistrap -a $ARCH -d $TARGET_DIR -f $OUTPUT_DIR/multistrap.conf
-	echo "deb [arch=$ARCH] $MIRROR $SUITE main" > $TARGET_DIR/etc/apt/sources.list.d/multistrap-debian.list
+echo "download and install "$PACKAGES". it will take a while. log will be saved in $OUTPUT_DIR/log"
+proot -0 multistrap -f $OUTPUT_DIR/multistrap.conf >>$OUTPUT_DIR/log
+if [ $? -ne 0 ]; then
+	echo "Failed!!!"
+	tail -n 100 $OUTPUT_DIR/log
+	exit 1
 else
-	proot -0 multistrap -a $ARCH -d $TARGET_DIR --no-auth -f $OUTPUT_DIR/multistrap.conf
+	echo "done!"
 fi
-
 cp $SCRIPTS_DIR/debconfseed.txt $TARGET_DIR/tmp/
 $CHROOTQEMUCMD $TARGET_DIR debconf-set-selections /tmp/debconfseed.txt
-
 for script in $TARGET_DIR/var/lib/dpkg/info/*.preinst; do
-        [ "$script" = "$TARGET_DIR/var/lib/dpkg/info/vpnc.preinst" ] && continue
-        DPKG_MAINTSCRIPT_NAME=preinst \
-        DPKG_MAINTSCRIPT_PACKAGE="`basename $script .preinst`" \
-        $CHROOTQEMUCMD $TARGET_DIR ${script##$TARGET_DIR} install
+DPKG_MAINTSCRIPT_NAME=preinst DPKG_MAINTSCRIPT_PACKAGE="`basename $script .preinst`" $CHROOTQEMUCMD $TARGET_DIR ${script##$TARGET_DIR} install
 done
-
 $CHROOTQEMUCMD $TARGET_DIR /usr/bin/dpkg --configure -a
 $SCRIPTS_DIR/fix_link.sh $TARGET_DIR/usr/lib/$TOOLCHAIN
 
