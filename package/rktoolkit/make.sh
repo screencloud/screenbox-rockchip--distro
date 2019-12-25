0c3a6cf559e8fb89a0b4eacdbf183f98f0f66570
Zain Wang <wzz@rock-chips.com>
2019-12-25 09:51:01 +0800

    package/rktoolkit: add vendor_storage to rktoolkit
    Change-Id: I253180735f019d4c03a7b848a3c9d7790601e629
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- package/rktoolkit/make.sh
+++ package/rktoolkit/make.sh
@@ -3,3 +3,4 @@
 set -e
 $GCC $TOP_DIR/external/rktoolkit/io.c --sysroot=$SYSROOT_DIR -I$SYSROOT_DIR/usr/include -I$SYSROOT_DIR/usr/include/$TOOLCHAIN -o $TARGET_DIR/usr/bin/io
 $GCC $TOP_DIR/external/rktoolkit/update.c $TOP_DIR/external/rktoolkit/update_recv/update_recv.c --sysroot=$SYSROOT_DIR -I$SYSROOT_DIR/usr/include -I$SYSROOT_DIR/usr/include/$TOOLCHAIN -I$TOP_DIR/external/rktoolkit/update_recv/ -o $TARGET_DIR/usr/bin/update
+$GCC $TOP_DIR/external/rktoolkit/vendor_storage.c --sysroot=$SYSROOT_DIR -I$SYSROOT_DIR/usr/include -I$SYSROOT_DIR/usr/include/$TOOLCHAIN -o $TARGET_DIR/usr/bin/vendor_storage
