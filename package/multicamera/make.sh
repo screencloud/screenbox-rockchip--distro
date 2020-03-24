84e9e4105e9ab55bbcea2495104712b5832037f0
zain wang <wzz@rock-chips.com>
2020-03-24 08:45:21 +0800

    package: multicamera: remove rkisp dependency
    Signed-off-by: zain wang <wzz@rock-chips.com>
    Change-Id: Id79e5c068dcb0ed21e3e4fd10e504f3c273b50dc

--- package/multicamera/make.sh
+++ package/multicamera/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DEPENDENCIES="weston libqt5widgets5 libatomic1 qtwayland5 libqt5multimedia5 libqt5multimediawidgets5 camera_engine_rkisp"
+DEPENDENCIES="weston libqt5widgets5 libatomic1 qtwayland5 libqt5multimedia5 libqt5multimediawidgets5"
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE "$DEPENDENCIES"
 PKG=multicamera
 #QMAKE=/usr/bin/qmake
